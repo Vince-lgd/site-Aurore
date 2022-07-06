20cce744c7bd1f5226c94a51cdbc392a490ce621
Vincent Lagarde <lagardevincent64@gmail.com>
2022-07-06 13:28:48 +0200

    first push

---
+++ src/components/Categorys.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import carousels from "../data/carousels";
+import Category from "./Category";
+
+const Categorys = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2400,
+    pauseOnHover: true,
+    ToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 580,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <div className="categorys">
+      <Slider {...settings}>
+        {carousels &&
+          carousels.map((category, index) => (
+            <Category key={index} {...category} />
+          ))}
+      </Slider>
+    </div>
+  );
+};
+export default Categorys;
---
+++ src/data/carousels.js
@@ -0,0 +1,25 @@
+const carousels = [
+  {
+    id: 1,
+    name: "Top",
+    image: "top.png",
+  },
+  {
+    id: 2,
+    name: "Pantalon",
+    image: "pant.png",
+  },
+  {
+    id: 3,
+    name: "Chemise",
+    image: "chemise.png",
+  },
+
+  {
+    id: 4,
+    name: "Accessoire",
+    image: "acce.png",
+  },
+];
+
+export default carousels;
